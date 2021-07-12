884a9ccc2c5396bed67532b87d4a139a9bb9117b
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-07-12 11:28:00 +0530

    call the setOnClickListener() method along with the object registerButton

--- app/src/main/java/com/example/googlefirebaseauth_java/MainActivity.java
+++ app/src/main/java/com/example/googlefirebaseauth_java/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.text.InputType;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
@@ -19,6 +20,13 @@ public class MainActivity extends AppCompatActivity {
         passwordField = findViewById(R.id.editText2);
         passwordField.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_PASSWORD);
         registerButton = findViewById(R.id.button);
+
+        registerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
 
 
     }
