beeb42dd225c7322c9ec5c00c3684ec4c4fb41b9
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-07-12 11:27:36 +0530

    call the setInputType() method along with the object passwordField

--- app/src/main/java/com/example/googlefirebaseauth_java/MainActivity.java
+++ app/src/main/java/com/example/googlefirebaseauth_java/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.googlefirebaseauth_java;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.InputType;
 import android.widget.Button;
 import android.widget.EditText;
 
@@ -16,6 +17,9 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         emailField = findViewById(R.id.editText);
         passwordField = findViewById(R.id.editText2);
+        passwordField.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_PASSWORD);
         registerButton = findViewById(R.id.button);
+
+
     }
 }
