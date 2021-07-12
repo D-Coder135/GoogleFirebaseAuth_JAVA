7926a97d2a1897cc8e2ea6f539ea51717068e046
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-07-12 11:35:10 +0530

    add a return keyword inside the else block body

--- app/src/main/java/com/example/googlefirebaseauth_java/MainActivity.java
+++ app/src/main/java/com/example/googlefirebaseauth_java/MainActivity.java
@@ -32,6 +32,7 @@ public class MainActivity extends AppCompatActivity {
                     return;
                 } else {
                     passwordField.setError("Please Enter Your Password.");
+                    return;
                 }
             }
         });
