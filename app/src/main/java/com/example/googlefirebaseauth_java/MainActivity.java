3a0c519091983d88ebf363ba70b31a41075b83c6
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-07-12 11:25:41 +0530

    define the object for the class Button

--- app/src/main/java/com/example/googlefirebaseauth_java/MainActivity.java
+++ app/src/main/java/com/example/googlefirebaseauth_java/MainActivity.java
@@ -3,10 +3,12 @@ package com.example.googlefirebaseauth_java;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
     EditText emailField, passwordField;
+    Button registerButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
