17ed0df594700479ebf27897c253d8fdfeef8b1d
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-07-12 11:25:21 +0530

    define the objects for the class EditText

--- app/src/main/java/com/example/googlefirebaseauth_java/MainActivity.java
+++ app/src/main/java/com/example/googlefirebaseauth_java/MainActivity.java
@@ -3,8 +3,10 @@ package com.example.googlefirebaseauth_java;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+    EditText emailField, passwordField;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
