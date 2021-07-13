9d8673f0f3ff88c58733332e590072743bfebfa0
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-07-13 22:41:10 +0530

    call the finish() method

--- app/src/main/java/com/example/googlefirebaseauth_java/WelcomePage.java
+++ app/src/main/java/com/example/googlefirebaseauth_java/WelcomePage.java
@@ -26,6 +26,7 @@ public class WelcomePage extends AppCompatActivity {
                 firebaseAuth.signOut();
                 Intent intent = new Intent(WelcomePage.this, MainActivity.class);
                 startActivity(intent);
+                finish();
             }
         });
     }
