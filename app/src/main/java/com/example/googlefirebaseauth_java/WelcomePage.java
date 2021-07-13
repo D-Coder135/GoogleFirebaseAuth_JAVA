23b7222784b5b941a93a94e7b505c7fb8a92f8a8
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-07-13 22:41:48 +0530

    optimize imports

--- app/src/main/java/com/example/googlefirebaseauth_java/WelcomePage.java
+++ app/src/main/java/com/example/googlefirebaseauth_java/WelcomePage.java
@@ -1,11 +1,10 @@
 package com.example.googlefirebaseauth_java;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.firebase.auth.FirebaseAuth;
 
