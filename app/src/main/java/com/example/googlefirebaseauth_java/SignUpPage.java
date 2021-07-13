565ae80da137a564817f49944da9aed289c8e7f1
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-07-13 22:25:48 +0530

    refactor the name for the objects of Button class

--- app/src/main/java/com/example/googlefirebaseauth_java/SignUpPage.java
+++ app/src/main/java/com/example/googlefirebaseauth_java/SignUpPage.java
@@ -12,7 +12,7 @@ import com.google.firebase.auth.FirebaseAuth;
 
 public class SignUpPage extends AppCompatActivity {
     EditText emailField, passwordField;
-    Button loginButton, signupButton;
+    Button registerButton, backButton;
     ProgressBar progressBar;
     FirebaseAuth firebaseAuth;
 
@@ -23,8 +23,8 @@ public class SignUpPage extends AppCompatActivity {
         emailField = findViewById(R.id.editText2);
         passwordField = findViewById(R.id.editText4);
         passwordField.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_PASSWORD);
-        loginButton = findViewById(R.id.button3);
-        signupButton = findViewById(R.id.button2);
+        registerButton = findViewById(R.id.button3);
+        backButton = findViewById(R.id.button2);
         progressBar = findViewById(R.id.progressBar2);
         firebaseAuth = FirebaseAuth.getInstance();
     }
