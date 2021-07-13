847c668cbda23cd702a47535237cd2cd318a82e7
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-07-13 22:23:07 +0530

    copy paste some part of the code along with some fixup

--- app/src/main/java/com/example/googlefirebaseauth_java/SignUpPage.java
+++ app/src/main/java/com/example/googlefirebaseauth_java/SignUpPage.java
@@ -3,12 +3,29 @@ package com.example.googlefirebaseauth_java;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.InputType;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class SignUpPage extends AppCompatActivity {
+    EditText emailField, passwordField;
+    Button loginButton, signupButton;
+    ProgressBar progressBar;
+    FirebaseAuth firebaseAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up_page);
+        emailField = findViewById(R.id.editText3);
+        passwordField = findViewById(R.id.editText4);
+        passwordField.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_PASSWORD);
+        loginButton = findViewById(R.id.button3);
+        signupButton = findViewById(R.id.button2);
+        progressBar = findViewById(R.id.progressBar2);
+        firebaseAuth = FirebaseAuth.getInstance();
     }
 }
